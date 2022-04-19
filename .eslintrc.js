d307e1748ab9c6cfb90ffed244e8fa5915400edf
Alexander Hofer <Alexander.Hofer@abf.ch>
2022-04-19 18:05:38 +0200

    Updated eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,11 +18,14 @@ module.exports = {
   plugins: [
     'react',
     '@typescript-eslint',
+    'react-hooks',
   ],
   rules: {
     'react/jsx-filename-extension': 'off',
     'consistent-return': 'off',
     'no-plusplus': 'off',
     'jsx-a11y/media-has-caption': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
   },
 };
